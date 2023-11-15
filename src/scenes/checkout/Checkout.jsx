67da7fca3b8064164a953c689c55e7ade1d1513b
Kostiantyn1303 <kosty13humera@gmail.com>
2023-11-15 14:18:49 +0200

    checkout

--- src/scenes/checkout/Checkout.jsx
+++ src/scenes/checkout/Checkout.jsx
@@ -47,7 +47,7 @@ const Checkout = () => {
       })),
     };
 
-    const response = await fetch("https://e-commerce-uxgn.onrender.com/api/orders", {
+    const response = await fetch("https://abrams.onrender.com/api/orders", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(requestBody),
