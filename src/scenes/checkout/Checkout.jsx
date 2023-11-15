e348cbf56c9d0a53f2c3536f4f3681cf5d9e6dc3
Kostiantyn1303 <kosty13humera@gmail.com>
2023-11-15 15:58:18 +0200

    1

--- src/scenes/checkout/Checkout.jsx
+++ src/scenes/checkout/Checkout.jsx
@@ -52,6 +52,7 @@ const Checkout = () => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(requestBody),
     });
+
     const session = await response.json();
     await stripe.redirectToCheckout({
       sessionId: session.id,
