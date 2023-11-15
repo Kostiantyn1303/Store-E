5d23fddde9289fc9a75655abcbd27b65fed0d7ee
Kostiantyn1303 <kosty13humera@gmail.com>
2023-11-15 17:12:41 +0200

    change

--- src/scenes/home/ShoppingList.jsx
+++ src/scenes/home/ShoppingList.jsx
@@ -18,7 +18,7 @@ function ShoppingList() {
   };
   async function getItems() {
     const items = await fetch(
-      "http://localhost:1337/api/items?populate=image",
+      "https://abrams.onrender.com/api/items?populate=image",
       { method: "GET" }
     );
     const itemJson = await items.json();
